8f29b3ff13374d4fb6014a2aa698711fc28801f3
Miguel <miguelruizramos96@gmail.com>
2018-04-24 16:18:30 +0200

    1er Prototipo App TPA

---
+++ app/src/main/java/com/teleco/pruebatpa/ListaActivity.java
@@ -0,0 +1,15 @@
+package com.teleco.pruebatpa;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class ListaActivity extends Activity {
+    private ListView lista;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+
+    }
+}
